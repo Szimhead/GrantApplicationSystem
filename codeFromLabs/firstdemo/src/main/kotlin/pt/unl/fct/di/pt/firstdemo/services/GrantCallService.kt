01985f089006a00b926c8379a56761b208524354
Tiago Costa <tiagocosta98@gmail.com>
2020-10-03 20:32:12 +0100

    panel and application handling in grantCall service

--- codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/services/GrantCallService.kt
+++ codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/services/GrantCallService.kt
@@ -1,6 +1,11 @@
 package pt.unl.fct.di.pt.firstdemo.services
 
 import org.springframework.stereotype.Service
+import pt.unl.fct.di.pt.firstdemo.api.ApplicationDTO
+import pt.unl.fct.di.pt.firstdemo.api.GrantCallDTO
+import pt.unl.fct.di.pt.firstdemo.api.PanelDTO
+import pt.unl.fct.di.pt.firstdemo.api.ReviewerDTO
+import java.util.*
 
 @Service
 class GrantCallService {
@@ -19,6 +24,27 @@ class GrantCallService {
     }
 
     fun deleteCall(title: String) {
+        TODO("Not yet implemented")
+    }
+
+    fun getPanel() = PanelDTO(
+            0,
+            "grant call title",
+            listOf<ReviewerDTO>(ReviewerDTO(0, "John"))
+    )
+
+    fun addPanel(panelId: Long) {
+        TODO("Not yet implemented")
+    }
+
+    fun getCallApplications(title: String) = listOf<ApplicationDTO>(
+            ApplicationDTO(1, Date(), 0),
+            ApplicationDTO(2, Date(), 1)
+    )
+
+    fun getOneCallApplication(title: String, id: Long) = ApplicationDTO(id, Date(), 0)
+
+    fun addApplication(title: String, id: Long) {
         TODO("Not yet implemented")
     }
 
