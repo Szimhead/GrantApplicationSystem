ab45e270ff85fc5c06d5ae4188548e24cbda8d10
Szymon <s.zmijewski@campus.fct.unl.pt>
2020-10-04 21:16:14 +0100

    CV

---
+++ codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/services/CVService.kt
@@ -0,0 +1,14 @@
+package pt.unl.fct.di.pt.firstdemo.services
+
+import org.springframework.stereotype.Service
+
+@Service
+class CVService {
+    fun getOne(id:Long )  {
+        TODO("Not yet implemented")
+    }
+
+    fun deleteCV(id:Long) {
+        TODO("Not yet implemented")
+    }
+}
