d6d80e7ad6b4c253fd897bb3c96b262e61c7e1e0
Tiago Costa <tiagocosta98@gmail.com>
2020-10-08 21:57:33 +0100

    new DTOs with correct attributes

--- codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/api/DTO.kt
+++ codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/api/DTO.kt
@@ -2,22 +2,22 @@ package pt.unl.fct.di.pt.firstdemo.api
 
 import java.util.Date
 
+data class UserDTO(val id: Long, val name: String, val email: String, val address: String)
+
+data class OrganizationDTO(val id: Long, val name: String, val contact: String)
+
+data class GrantCallDTO(val title:String, val description: String, val funding: Float, val openDate: Date, val closeDate: Date)
+
 data class ApplicationDTO(val id:Long, val submissionDate: Date, val status:Int)
-
-data class GrantCallDTO(val title:String)
-
-data class InstitutionDTO(val id:Long, val name:String, val contact: String)
-
-data class StudentDTO(val studentNr:Long, val name:String)
-
-data class ReviewerDTO(val ReviewerNr:Long, val name:String)
-
-data class FinalDTO(val id:Long, val grade:String, val review: String)
-
-data class PanelDTO(val panelId:Long, val grantTitle:String, val reviewers: List<ReviewerDTO>)
 
 data class ReviewDTO(val id:Long, val isAccepted: Boolean, val comment: String)
 
-data class SponsorDTO(val id:Long, val name: String, val contact: String)
+data class DataItemDTO(val name: String, val datatype: String, val isMandatory: Boolean)
 
-data class CVDTO(val id:Long)
+data class CVRequirementDTO(val name: String, val datatype: String, val isMandatory: Boolean)
+
+data class AnswerDTO(val value: String, val datatype: String)
+
+data class CVItemDTO(val value: String, val datatype: String)
+
+data class CVDTO(val items: List<CVItemDTO>)
