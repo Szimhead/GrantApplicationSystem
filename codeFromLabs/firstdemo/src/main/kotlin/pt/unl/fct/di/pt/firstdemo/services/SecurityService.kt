d32674b0a7765328b15e032529c7de7c284b2a02
Szymon <s.zmijewski@campus.fct.unl.pt>
2020-10-29 22:19:18 +0000

    SecurityService - methods regarding application (with errors)

--- codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/services/SecurityService.kt
+++ codeFromLabs/firstdemo/src/main/kotlin/pt/unl/fct/di/pt/firstdemo/services/SecurityService.kt
@@ -8,46 +8,52 @@ import pt.unl.fct.di.pt.firstdemo.model.ApplicationRepository
 @Component
 class SecurityService(val applications: ApplicationRepository) {
 
-    fun canAddApplication( user: UserDAO){
+    fun canAddApplication(user: UserDAO){
         //only Student role
     }
 
-    fun canEditApplication( student: StudentDAO, applicationId: Long){
+    fun canEditApplication( student: StudentDAO, applicationId: Long): Boolean {
         val application = applications.findById(applicationId).orElseThrow{
             NotFoundException("chuj")
         }
         //TODO - this should not throw any exception, but otherwise function contains() can't be called
-        application!=null && student.applications.contains(application)
+        return application!=null && student.applications.contains(application)
         //and has a role Student
     }
 
-    fun canDeleteApplication( student: StudentDAO, applicationId: Long){
-        canEditApplication(student,applicationId)
+    fun canDeleteApplication(student: StudentDAO, applicationId: Long){
+        return canEditApplication(student,applicationId)
         //and has a role Student
     }
 
     //TODO------------
     //---------------- two methods or one with UserDAO, but it's more complicated - to be decided
     fun canGetApplication( student: StudentDAO, applicationId: Long){
-        canEditApplication(student,applicationId)
+        return canEditApplication(student,applicationId)
         //and has a role Student
     }
 
-    fun canGetApplication( reviewer: ReviewerDAO, applicationId: Long){
+    fun canGetApplication(reviewer: ReviewerDAO, applicationId: Long): Boolean {
         val application = applications.findById(applicationId)
 
-
+        return application !=null &&  reviewer.panels.contains(application.panel)
         //and has a role Student
     }
     //---------------
 
-    fun canGetStudentApplications( student: StudentDAO){
-        
-        //and has a role Student
+    fun canGetStudentApplications(student: StudentDAO){
+
+        //has a role Student and is the owner of the applications
     }
 
-    fun canGetGrantApplications( call: GrantCallDAO){
+    fun canGetGrantApplications(sponsor: SponsorDAO, call: GrantCallDAO): Boolean {
+        return call.sponsor.equals(sponsor)
+        //has a role sponsor
+    }
 
+    fun canGetGrantApplications(reviewer: ReviewerDAO, call: GrantCallDAO): Boolean? {
+        return call.panel !=null && call.panel!!.reviewers !=null && call.panel?.reviewers?.contains(reviewer)!!
+        //has a role reviewer
     }
 
 
