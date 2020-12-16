e3d07316be7cbb24d8512b24473b63e66cf221d5
Weronika Strączek <w.straczek@campus.fct.unl.pt>
2020-12-16 18:28:43 +0000

    header

--- codeFromLabs/firstdemo/grantClient/grantclient/src/components/header.tsx
+++ codeFromLabs/firstdemo/grantClient/grantclient/src/components/header.tsx
@@ -9,24 +9,25 @@ export const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" className="p-3">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Nav.Item><img
+                    style={{ width: 50 }}
+                    className="mr-3"
+                    src="/images/logo.png"
+                    alt="Logo"
+                /></Nav.Item>
+                <Navbar.Brand href="#home">Grant Application System</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    </Nav>
+                    <Nav className="mr-4">
+                        <NavDropdown title="Profile" id="basic-nav-dropdown" className="mr-5">
+                            <NavDropdown.Item href="#action/3.1">My profile</NavDropdown.Item>
                             <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
                 </Navbar.Collapse>
             </Navbar>
         </header>
