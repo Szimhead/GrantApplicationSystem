8fb6aa7ae1dc972bc53e29e7b3cedacaac6ee7da
Tiago Costa <tiagocosta98@gmail.com>
2020-12-17 03:17:14 +0000

    added student homepage (empty) and react router to deal with page links

--- codeFromLabs/firstdemo/grantClient/grantclient/src/index.tsx
+++ codeFromLabs/firstdemo/grantClient/grantclient/src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import StudentHomepage from "./StudentHomepage";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <Route exact path={"/"} component={App} />
+            <Route path={"/studentHomepage"} component={StudentHomepage} />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
